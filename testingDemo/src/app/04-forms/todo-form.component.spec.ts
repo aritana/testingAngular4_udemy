b1f9507b5eddd5392e6625c3770061a66550270f
aritana noara <arinoara@yahoo.com.br>
2024-09-03 20:15:28 -0300

    test: form controls test

--- testingDemo/src/app/04-forms/todo-form.component.spec.ts
+++ testingDemo/src/app/04-forms/todo-form.component.spec.ts
@@ -1,15 +1,24 @@
+import { FormBuilder } from '@angular/forms';
 import { TodoFormComponent } from './todo-form.component'; 
 
 describe('TodoFormComponent', () => {
   var component: TodoFormComponent; 
 
   beforeEach(() => {
-
+    component = new TodoFormComponent(new FormBuilder());
   });
 
-  it('', () => {
+  it('should create a form with 2 controls', () => {
+    expect(component.form.contains('name')).toBe(true);
+    expect(component.form.contains('email')).toBe(true);
   });
 
-  it('', () => {
+  it('should make the name control required', () => {
+    //Arrange
+    let control  = component.form.get('name');
+    //Act
+    control?.setValue('');
+    //Arrange
+    expect(control?.valid).toBeFalsy();  
   });
 });
