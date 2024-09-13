7aec6a10f25470497f5f61a7464d4a2e5bdc7d9e
Aritana Noara Santos <arinoara@yahoo.com.br>
2024-09-13 02:21:17 -0300

    test: working with spies

--- testingDemo/src/app/05-event-emitters/vote.component.spec.ts
+++ testingDemo/src/app/05-event-emitters/vote.component.spec.ts
@@ -1,12 +1,20 @@
-import { VoteComponent } from './vote.component'; 
+import { VoteComponent } from './vote.component';
 
 describe('VoteComponent', () => {
-  var component: VoteComponent; 
+  var component: VoteComponent;
 
   beforeEach(() => {
     component = new VoteComponent();
   });
 
-  it('', () => {
+  it('should raise voteChanged event when upvoted', () => {
+    let totalVotes:number = 0
+    component.voteChanged.subscribe((newVoteCount: number) => {
+      totalVotes = newVoteCount;
+    });
+
+    component.upVote();
+
+    expect(totalVotes).toBe(1);
   });
 });
