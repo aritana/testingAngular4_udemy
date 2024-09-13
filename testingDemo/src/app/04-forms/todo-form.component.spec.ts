de63ce610498abcf40068d0331f25438710163eb
Aritana Noara Santos <arinoara@yahoo.com.br>
2024-09-13 01:44:48 -0300

    test: working with forms

--- testingDemo/src/app/04-forms/todo-form.component.spec.ts
+++ testingDemo/src/app/04-forms/todo-form.component.spec.ts
@@ -1,15 +1,27 @@
-import { TodoFormComponent } from './todo-form.component'; 
+import { FormBuilder } from '@angular/forms';
+import { TodoFormComponent } from './todo-form.component';
 
 describe('TodoFormComponent', () => {
-  var component: TodoFormComponent; 
+  let component: TodoFormComponent;
+  let formbuilder: FormBuilder;
 
   beforeEach(() => {
+    formbuilder = new FormBuilder();
+    component = new TodoFormComponent(formbuilder);
+  });
+
+  it('should contains name', () => {
+    expect(component.form.contains('name')).toBe(true);
 
   });
 
-  it('', () => {
+  it('should make the name required', () => {
+    component.form.get('name')?.setValue('');
+    expect(component.form.get('name')?.valid).toBeFalsy();
+
   });
 
-  it('', () => {
+  it('should contains email', () => {
+    expect(component.form.contains('email')).toBe(true);
   });
 });
